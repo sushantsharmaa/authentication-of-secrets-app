cb27e5df3b4f53d958ff227b2ed0ec1301f702cf
Sushant Sharma <sushant.x01@gmail.com>
2022-02-17 22:36:28 +0530

    Level-2 : Database Encryption

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,10 +18,13 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
-const userSchema = {
+const userSchema =  new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
